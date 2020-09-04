541bb9d6bfb721b9350fa9a5ad81e9a3ab95b1ea
荣辰 <rongchen@eheluo.com>
2020-09-04 18:28:45 +0800

    搭建项目

---
+++ onism-service/src/main/java/pers/rong/onism/OnismApplication.java
@@ -0,0 +1,13 @@
+package pers.rong.onism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnismApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnismApplication.class, args);
+    }
+
+}
